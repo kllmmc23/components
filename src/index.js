ae236102ecac78e84c1ce0dcc1eb00ffa8df289d
Michael <kllmmc23@gmail.com>
2020-05-22 09:14:29 -0600

    custom-children-reuse

--- src/index.js
+++ src/index.js
@@ -1,15 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const rootElement = document.getElementById("root");
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="sam" timeAgo="Today at 4:40 pm" />
-      <CommentDetail author="Alex" timeAgo="Today at 5:40 pm" />
-      <CommentDetail author="Jane" timeAgo="Today at 6:40 pm" />
+      <ApprovalCard>
+        <div>
+          <h4>Important!</h4>
+          <p>Are you sure you want to proceed?</p>
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="sam"
+          timeAgo="Today at 4:40 pm"
+          comment="Nice Blog Post!"
+          imgAvatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 5:40 pm"
+          comment="Nice Blog Post!"
+          imgAvatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Today at 6:40 pm"
+          comment="Nice Blog Post!"
+          imgAvatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
