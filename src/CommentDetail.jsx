21caf2d76ecc7ed21a77737c89f78d5741cca6ad
Michael <kllmmc23@gmail.com>
2020-05-22 00:09:07 -0600

    passing-receiving-props

--- src/CommentDetail.jsx
+++ src/CommentDetail.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import faker from 'faker';
 
-const CommentDetail = () => {
-    return (
+const CommentDetail = (props) => {
+    return (     
         <div className="comment">
         <a href="/" className="avatar">
           <img alt="avatar" src={faker.image.avatar()} />
         </a>
         <div className="content">
           <a href="/" className="author">
-            Sam
+            {props.author}
           </a>
           <div className="metadata">
             <span className="date">Today at 12</span>
--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import CommentDetail from './CommentDetail'
+import CommentDetail from "./CommentDetail";
 
 const rootElement = document.getElementById("root");
 
 const App = () => {
-    return (
-      <div className="ui container comments">
-        <CommentDetail />
-        <CommentDetail />
-        <CommentDetail />
-        <CommentDetail />
-        <CommentDetail />
-      </div>
-    );
+  return (
+    <div className="ui container comments">
+      <CommentDetail author="sam" />
+      <CommentDetail author="Alex" />
+      <CommentDetail author="Jane" />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, rootElement);
